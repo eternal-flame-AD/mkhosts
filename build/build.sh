9f3a4ede8c94d69e4974fe1fcade314abc899113
eternal-flame-AD <eternal_flame-AD@protonmail.com>
2018-08-25 22:13:07 +0800

    use lzma for dist compression

--- build/build.sh
+++ build/build.sh
@@ -2,4 +2,4 @@ env GOOS=linux GOARCH=386 go build -o mkhosts-linux-386 -v github.com/eternal-fl
 env GOOS=linux GOARCH=amd64 go build -o mkhosts-linux-amd64 -v github.com/eternal-flame-AD/mkhosts
 env GOOS=windows GOARCH=386 go build -o mkhosts-windows-386.exe -v github.com/eternal-flame-AD/mkhosts
 env GOOS=windows GOARCH=amd64 go build -o mkhosts-windows-amd64.exe -v github.com/eternal-flame-AD/mkhosts
-upx --best mkhosts-*
+upx --lzma mkhosts-*

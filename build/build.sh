c99b3318b263f090ad4aa31914692a01f7643131
eternal-flame-AD <eternal_flame-AD@protonmail.com>
2018-10-10 13:39:07 +0800

    strip debugging info during build

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,5 @@
-env GOOS=linux GOARCH=386 go build -o mkhosts-linux-386 -v github.com/eternal-flame-AD/mkhosts
-env GOOS=linux GOARCH=amd64 go build -o mkhosts-linux-amd64 -v github.com/eternal-flame-AD/mkhosts
-env GOOS=windows GOARCH=386 go build -o mkhosts-windows-386.exe -v github.com/eternal-flame-AD/mkhosts
-env GOOS=windows GOARCH=amd64 go build -o mkhosts-windows-amd64.exe -v github.com/eternal-flame-AD/mkhosts
+env GOOS=linux GOARCH=386 go build -ldflags="-s -w" -o mkhosts-linux-386 -v github.com/eternal-flame-AD/mkhosts
+env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o mkhosts-linux-amd64 -v github.com/eternal-flame-AD/mkhosts
+env GOOS=windows GOARCH=386 go build -ldflags="-s -w" -o mkhosts-windows-386.exe -v github.com/eternal-flame-AD/mkhosts
+env GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o mkhosts-windows-amd64.exe -v github.com/eternal-flame-AD/mkhosts
 upx --lzma mkhosts-*
